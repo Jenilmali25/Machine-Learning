baac4486fcc96fba8891521985e889c038ae61c3
Jenilmali25 <jenilcodes25@gmail.com>
2025-07-26 22:44:41 +0530

    Variable rule, jinja2 template engine, learned building url (request, redirect, url_for)

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-Machine Learning
+# 📘 Daily Learning Progress – Data Science & Machine Learning
+
+This repository tracks my daily learning progress in:
+
+- 📊 Data Science  
+- 🤖 Machine Learning  
+- 🧠 Deep Learning  
+- 🗣️ Natural Language Processing (NLP)
+
+---
+
+## 📅 Daily Log
+
+-Flask applications (WSGI , Jinja2)
+-GET, POST
+-Variable rule, jinja2 template engine, learned building url (request, redirect, url_for)
--- jinja.py
+++ jinja.py
@@ -1,14 +1,40 @@
-#building url dynamically
-
-#jinja 2 template engine
 
 #variable rule
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 app=Flask(__name__)
 
 @app.route('/<int:value>')
 def a(value):
-    return f"the value is {value}"
+    res=""
+    if(value%2==0):
+        res=f"{value} is a Even number"
+    else:
+        res=f"{value} is a Odd Number"
+    return render_template('result.html', results=res)
+
+
+
+#jinja 2 template engine
+'''
+{{   }} expression to print output in html
+{%...%} Condiitions and For loops
+{#..#} comments
+'''
+@app.route('/result<int:value>')
+def result(value):
+    return render_template('result1.html', results=value)
+
+
+#building url dynamically
+@app.route('/submit', methods=['GET','POST'])
+def submit():
+    if request.method=="POST":
+        number = float(request.form['m'])
+    else:
+        return render_template('build_url.html')
+    return redirect(url_for('result', value=int(number)))
+
+
 
 if __name__ == "__main__":
-    app.run(debug="True")
+    app.run(debug=True)
